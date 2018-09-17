22015c1da25ebab1e76710ef8ad4738a76b71dd0
Peter Johnson <pjohnson@cs.hmc.edu>
2018-09-17 01:06:51 -0700

    FIxed  transfer function code in impulse_response.m and deleted unecessary Qfcn.m file

--- impulse_response.m
+++ impulse_response.m
@@ -5,11 +5,13 @@ function [ h,t ] = impulse_response( x, y, fs )
 %           y, measured output signal
 %           fs, sampling frequency of measurements
 %   outputs:    h, calculated impulse response
+% updated by Peter Johnson sep 17, 2018
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+plot(h); %plots the calculated impulse response
 
 end
 
